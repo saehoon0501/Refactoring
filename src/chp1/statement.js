878601e37ac0f5bd775ea56843271475b82a573a
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-07 22:39:36 +0900

    volumeCredits값 계산 코드 함수로 추출 + 변수 인라인

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -9,14 +9,17 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
 
   function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
