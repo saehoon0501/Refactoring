42be6547d26c96d67e28a935bb3bdb1264abc6a0
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-07 22:12:36 +0900

    switch문 함수 추출

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -10,8 +10,22 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+
+  function amountFor(perf, play) {
+    let thisAmount = 0;
     switch (play.type) {
       case "tragedy":
         thisAmount = 40000;
@@ -29,18 +43,8 @@ function statement(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
 }
 
 export { statement };
