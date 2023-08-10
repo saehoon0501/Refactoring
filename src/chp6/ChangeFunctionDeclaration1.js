b73116565cca37e65dde9dd661b4156b5c196d53
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-10 23:25:23 +0900

    간단한 절차

---
+++ src/chp6/ChangeFunctionDeclaration1.js
@@ -0,0 +1,5 @@
+function circumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+export { circumference };
--- test/chp6/ChangeFunctionDeclaration1.test.js
+++ test/chp6/ChangeFunctionDeclaration1.test.js
@@ -2,6 +2,6 @@ import { circumference } from "../../src/chp6/ChangeFunctionDeclaration1";
 
 describe("circumference", () => {
   it("should calculate circumference given a radius", () => {
-    expect(circumference(100)).to.equal(628.3185307179587);
+    expect(circumference(100)).toEqual(628.3185307179587);
   });
 });
