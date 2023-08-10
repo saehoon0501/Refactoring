063dbc5fb45fe8ef7c7373a77ed3ab0ed27aae58
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-10 23:22:54 +0900

    함수 선언 바꾸기 setup

---
+++ test/chp6/ChangeFunctionDeclaration1.test.js
@@ -0,0 +1,7 @@
+import { circumference } from "../../src/chp6/ChangeFunctionDeclaration1";
+
+describe("circumference", () => {
+  it("should calculate circumference given a radius", () => {
+    expect(circumference(100)).to.equal(628.3185307179587);
+  });
+});
