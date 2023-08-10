46aeb1fc5e344cd85e4b399575106f9b47b64c82
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-10 21:58:10 +0900

    함수 추출하기 연습

---
+++ src/chp6/PrintOwing.js
@@ -0,0 +1,22 @@
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocalDateString()}`);
+}
