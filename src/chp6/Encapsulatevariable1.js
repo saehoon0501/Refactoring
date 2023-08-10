912f63a18455bdf1f02241ea5a2a9601b93f64f0
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-10 23:57:02 +0900

    변수 캡슐화 마무리

--- src/chp6/Encapsulatevariable1.js
+++ src/chp6/Encapsulatevariable1.js
@@ -1,6 +1,6 @@
 let defaultOwner = { firstName: "Martin", lastName: "Fowler" };
 function getDefaultOwner() {
-  return defaultOwner;
+  return Object.assign({}, defaultOwner);
 }
 function setDefaultOwner({ firstName, lastName }) {
   defaultOwner = { firstName, lastName };
