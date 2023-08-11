68a82afcb03f374afa425397a293b6ac46d804f2
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-11 16:20:17 +0900

    컬렉션 캡슐화 setup

---
+++ src/chp7/EncapsulateCollection.js
@@ -0,0 +1,29 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+    this._courses = [];
+  }
+
+  get name() {
+    return this._name;
+  }
+  get courses() {
+    return this._courses;
+  }
+  set courses(aList) {
+    return (this._courses = aList);
+  }
+}
+class Course {
+  constructor(name, isAdvanced) {
+    this._name = name;
+    this._isAdvanced = isAdvanced;
+  }
+  get name() {
+    return this._name;
+  }
+  get isAdvanced() {
+    return this.this_isAdvanced;
+  }
+}
+export { Person, Course };
