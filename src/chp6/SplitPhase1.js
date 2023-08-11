3df52a7ecd25cc93d7df1d814c28f3ac3dc9aca6
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-11 14:48:06 +0900

    단계 쪼개기 마무리

--- src/chp6/SplitPhase1.js
+++ src/chp6/SplitPhase1.js
@@ -1,12 +1,16 @@
 export function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = { basePrice, discount, quantity };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice, discount, quantity };
 }
 
 function applyShipping(priceData, shippingMethod) {
