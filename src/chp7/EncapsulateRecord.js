77d00ea5891cb52fe924ebdbde62fed93e651853
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-11 15:54:54 +0900

    레코드 추출하기 마무리

--- src/chp7/EncapsulateRecord.js
+++ src/chp7/EncapsulateRecord.js
@@ -1,10 +1,33 @@
-const organization = { name: "Acme Gooseberries", country: "GB" };
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set name(aString) {
+    this._name = aString;
+  }
+
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
+}
+
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
 
 function getOrganization() {
   return organization;
 }
-
-let result = "";
-result = `<h1>${getOrganization().name}</h1>`;
 
 export { getOrganization };
