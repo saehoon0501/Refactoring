2af3c6ef21111d46d8f8c20db6ef4cb77c47cab4
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 17:55:40 +0900

    함수 옮기기 2 마무리

--- src/chp8/MoveFunction2.js
+++ src/chp8/MoveFunction2.js
@@ -11,11 +11,7 @@ export class Account {
   }
 
   overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this.daysOverdrawn * 1.75;
+    return this.type.overdraftCharge(this._daysOverdrawn);
   }
 
   get daysOverdrawn() {
@@ -31,5 +27,11 @@ export class AccountType {
     return this._type === "Premium";
   }
 
-  overdraftCharge(daysOverdrawn) {}
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + (daysOverdrawn - 7) * 0.85;
+    } else return daysOverdrawn * 1.75;
+  }
 }
