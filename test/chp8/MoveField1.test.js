81c18159ce260f5e7355dde75474d2f0f8a190b7
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 18:13:15 +0900

    필드 옮기기 1 setup

---
+++ test/chp8/MoveField1.test.js
@@ -0,0 +1,11 @@
+import { Customer } from "../../src/chp8/MoveField1";
+import Money from "js-money";
+
+describe("Customer", () => {
+  it("calculates discount", () => {
+    const customer = new Customer("Mike", 0.05);
+    expect(customer.applyDiscount(new Money(500, Money.EUR))).toEqual(
+      new Money(475, Money.EUR)
+    );
+  });
+});
