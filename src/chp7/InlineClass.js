84da67d691b66cb51fbaf974d0c5cc419566e973
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 16:33:55 +0900

    클래스 인라인하기 마무리

--- src/chp7/InlineClass.js
+++ src/chp7/InlineClass.js
@@ -1,11 +1,36 @@
-class TrackingInformation {
-  constructor(shippingCompany, trackingNumber) {
+// class TrackingInformation {
+//   constructor(shippingCompany, trackingNumber) {
+//     this._shippingCompany = shippingCompany;
+//     this._trackingNumber = trackingNumber;
+//   }
+//   get shippingCompany() {
+//     return this._shippingCompany;
+//   }
+//   set shippingCompany(arg) {
+//     this._shippingCompany = arg;
+//   }
+//   get trackingNumber() {
+//     return this._trackingNumber;
+//   }
+//   set trackingNumber(arg) {
+//     this._trackingNumber = arg;
+//   }
+//   get display() {
+//     return `${this.shippingCompany}: ${this.trackingNumber}`;
+//   }
+// }
+
+class Shipment {
+  constructor(trackingNumber, shippingCompany) {
     this._shippingCompany = shippingCompany;
     this._trackingNumber = trackingNumber;
   }
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
   get shippingCompany() {
     return this._shippingCompany;
-  } // 배송 회사
+  }
   set shippingCompany(arg) {
     this._shippingCompany = arg;
   }
@@ -14,27 +39,6 @@ class TrackingInformation {
   }
   set trackingNumber(arg) {
     this._trackingNumber = arg;
-  }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
-class Shipment {
-  constructor(trackingNumber, shippingCompany) {
-    this._trackingInformation = new TrackingInformation(
-      shippingCompany,
-      trackingNumber
-    );
-  }
-  get trackingInfo() {
-    return this._trackingInformation.display;
-  }
-  get trackingInformation() {
-    return this._trackingInformation;
-  }
-  set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation;
   }
 }
 export { Shipment };
