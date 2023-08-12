8535b5d0314c8dfa3399f0847adff9250326f771
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 15:26:19 +0900

    기본형을 객체로 캡슐화 마무리

--- src/chp7/ReplacePrimitiveWithObject.js
+++ src/chp7/ReplacePrimitiveWithObject.js
@@ -1,12 +1,41 @@
+class Priority {
+  constructor(value) {
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`<${value}> is invalid for Priority`);
+  }
+  toString() {
+    return this._value;
+  }
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+  equals(other) {
+    return this._index > other._index;
+  }
+  higherThan(other) {
+    return this._index > other._index;
+  }
+  lowerThan(other) {
+    return this._index < other._index;
+  }
+}
+
 class Order {
   constructor(data) {
-    this._priority = data.priority;
+    this._priority = new Priority(data.priority);
   }
   get priority() {
     return this._priority;
   }
+  get priorityString() {
+    return this._priority.toString();
+  }
   set priority(aString) {
-    this._priority = aString;
+    this._priority = new Priority(aString);
   }
 }
 
@@ -16,8 +45,8 @@ const orders = [
   new Order({ priority: "rush" }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => "high" === o.priority || "rush" === o.priority
+const highPriorityCount = orders.filter((o) =>
+  o.priority.higherThan(new Priority("normal"))
 ).length;
 
 export { highPriorityCount };
