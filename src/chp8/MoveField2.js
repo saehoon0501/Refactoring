3e6310bfead819bbc1648d23f1ec69c7fadf2d25
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 18:25:22 +0900

    필드 옮기기 2 마무리

--- src/chp8/MoveField2.js
+++ src/chp8/MoveField2.js
@@ -1,8 +1,9 @@
+import { assert } from "assert";
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    this._interestRate = interestRate;
+    assert(interestRate === this._type.interestRate);
   }
 
   get interestRate() {
@@ -11,7 +12,11 @@ class Account {
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+  get interestRate() {
+    return this._interestRate;
   }
 }
