70e0d2c56458972776803f7974298b1f42262012
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 17:44:49 +0900

    함수 옮기기 1 마무리

--- src/chp8/MoveFunction1.js
+++ src/chp8/MoveFunction1.js
@@ -1,10 +1,9 @@
 export function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = top_calculateDistance(points);
-  const pace = totalTime / 60 / totalDistance;
+  const pace = totalTime / 60 / totalDistance(points);
   return {
     time: totalTime,
-    distance: totalDistance,
+    distance: totalDistance(points),
     pace: pace,
   };
 
@@ -12,37 +11,30 @@ export function trackSummary(points) {
     //just a hard code number
     return 10000;
   }
-  function calculateDistance(points) {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
 }
 
-function top_calculateDistance(points) {
+function totalDistance(points) {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
   }
   return result;
+}
 
-  function distance(p1, p2) {
-    // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
-    const EARTH_RADIUS = 3959; // in miles
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(radians(p2.lat)) *
-        Math.cos(radians(p1.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-  }
+function distance(p1, p2) {
+  // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
+  const EARTH_RADIUS = 3959; // in miles
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(radians(p2.lat)) *
+      Math.cos(radians(p1.lat)) *
+      Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return EARTH_RADIUS * c;
+}
 
-  function radians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
+function radians(degrees) {
+  return (degrees * Math.PI) / 180;
 }
