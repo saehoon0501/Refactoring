5fe84ffe8d068f37a0079c1c3035113ee3ba424f
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 16:07:03 +0900

    클래스 추출하기 마무리

--- src/chp7/ExtractClass.js
+++ src/chp7/ExtractClass.js
@@ -1,8 +1,29 @@
+class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
+  get areaCode() {
+    return this._areaCode;
+  }
+  get number() {
+    return this._number;
+  }
+  set areaCode(arg) {
+    this._areaCode = arg;
+  }
+  set number(arg) {
+    this._number = arg;
+  }
+  toString() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+}
+
 class Person {
   constructor(name, officeAreaCode, officeNumber) {
     this._name = name;
-    this._officeAreaCode = officeAreaCode;
-    this._officeNumber = officeNumber;
+    this._telephoneNumber = new TelephoneNumber(officeAreaCode, officeNumber);
   }
   get name() {
     return this._name;
@@ -11,19 +32,19 @@ class Person {
     this._name = arg;
   }
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    return this._telephoneNumber.toString();
   }
   get officeAreaCode() {
-    return this._officeAreaCode;
+    return this._telephoneNumber.areaCode;
   }
   set officeAreaCode(arg) {
-    this._officeAreaCode = arg;
+    this._telephoneNumber.areaCode = arg;
   }
   get officeNumber() {
-    return this._officeNumber;
+    return this._telephoneNumber.number;
   }
   set officeNumber(arg) {
-    this._officeNumber = arg;
+    this._telephoneNumber.number = arg;
   }
 }
 
