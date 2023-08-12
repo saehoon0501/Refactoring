6e4eb8fb9703467858453cfe621592e49d778a5f
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-12 17:40:01 +0900

    함수 옮기기 setup

---
+++ test/chp8/MoveFunctions1.test.js
@@ -0,0 +1,21 @@
+import { trackSummary } from "../../src/chp8/MoveFunction1";
+
+describe("trackSummary", () => {
+  it("calculate distance, pace and time between two points", () => {
+    const newYork = {
+      lat: 40.73061,
+      lon: -73.935242,
+    };
+
+    const tokyo = {
+      lat: 35.652832,
+      lon: 139.839478,
+    };
+
+    expect(trackSummary([newYork, tokyo])).to.eql({
+      distance: 6740.914927144901,
+      pace: 0.02472463581991205,
+      time: 10000,
+    });
+  });
+});
