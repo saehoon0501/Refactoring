978eabf84083fc05d1d2ffd9748616d350b24981
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-13 15:31:25 +0900

    반복문 쪼개고 부수효과 없애기

--- src/chp8/SplitLoop.js
+++ src/chp8/SplitLoop.js
@@ -2,8 +2,11 @@ export function reportYoungestAgeAndTotalSalary(people) {
   let youngest = people[0] ? people[0].age : Infinity;
   let totalSalary = 0;
   for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
     totalSalary += p.salary;
+  }
+
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
   }
 
   return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
