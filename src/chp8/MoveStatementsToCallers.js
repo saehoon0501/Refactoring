9222606803e21c36df07d42138514bb0f9966c6f
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-13 15:05:15 +0900

    문장을 호출한 곳으로 옮기기 마무리

--- src/chp8/MoveStatementsToCallers.js
+++ src/chp8/MoveStatementsToCallers.js
@@ -1,7 +1,7 @@
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
-  zztmp(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
   outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
@@ -10,19 +10,13 @@ function listRecentPhotos(outStream, photos) {
     .filter((p) => p.date > recentDateCutoff())
     .forEach((p) => {
       outStream.write("<div>\n");
-      zztmp(outStream, p);
+      emitPhotoData(outStream, p);
       outStream.write(`<p>location: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
 }
 
 function emitPhotoData(outStream, photo) {
-  outStream.write(`<p>title: ${photo.title}</p>\n`);
-  outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
-}
-
-function zztmp(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
 }
