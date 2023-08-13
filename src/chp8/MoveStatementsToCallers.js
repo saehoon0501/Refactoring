35378dc70c88df86b831d3feb3f52e217d3bd5aa
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-13 15:04:16 +0900

    남길 내용 함수 인라인

--- src/chp8/MoveStatementsToCallers.js
+++ src/chp8/MoveStatementsToCallers.js
@@ -1,7 +1,8 @@
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
-  emitPhotoData(outStream, person.photo);
+  zztmp(outStream, person.photo);
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -9,7 +10,8 @@ function listRecentPhotos(outStream, photos) {
     .filter((p) => p.date > recentDateCutoff())
     .forEach((p) => {
       outStream.write("<div>\n");
-      emitPhotoData(outStream, p);
+      zztmp(outStream, p);
+      outStream.write(`<p>location: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
 }
