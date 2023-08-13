544e88122bcf83a50a426c8c485749a2cc595f3d
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-13 15:30:53 +0900

    반복문 쪼개기 setup

---
+++ test/chp8/SplitLoop.test.js
@@ -0,0 +1,24 @@
+import { reportYoungestAgeAndTotalSalary } from "../../src/chp8/SplitLoop";
+
+const people = [
+  {
+    age: "30",
+    salary: 2000,
+  },
+  {
+    age: "25",
+    salary: 2000,
+  },
+  {
+    age: "31",
+    salary: 2000,
+  },
+];
+
+describe("reportYoungestAgeAndTotalSalary", () => {
+  it("report youngest age and total salary", () => {
+    expect(reportYoungestAgeAndTotalSalary(people)).toEqual(
+      "youngestAge: 25, totalSalary: 6000"
+    );
+  });
+});
