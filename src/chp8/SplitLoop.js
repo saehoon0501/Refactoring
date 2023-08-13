2d67428ba3c604e33540be1baf7ffcc6f5a3392f
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-13 15:37:38 +0900

    반복문을 파이프라인 만들기 + 알고리즘 교체하기

--- src/chp8/SplitLoop.js
+++ src/chp8/SplitLoop.js
@@ -7,17 +7,9 @@ export function reportYoungestAgeAndTotalSalary(people) {
 }
 
 function ztotalSalary(people) {
-  let result = 0;
-  for (const p of people) {
-    result += p.salary;
-  }
-  return result;
+  return people.reduce((total, p) => total + p.salary, 0);
 }
 
 function youngestAge(people) {
-  let result = people[0] ? people[0].age : Infinity;
-  for (const p of people) {
-    if (p.age < result) result = p.age;
-  }
-  return result;
+  return Math.min(...people.map((p) => p.age));
 }
