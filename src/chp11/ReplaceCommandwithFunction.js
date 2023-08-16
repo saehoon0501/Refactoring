851aa2a395193059a9b2f4757b7a2a9629b82eff
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-16 23:36:41 +0900

    명령을 함수로 바꾸기 마무리

--- src/chp11/ReplaceCommandwithFunction.js
+++ src/chp11/ReplaceCommandwithFunction.js
@@ -1,13 +1,17 @@
-export class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+// export class ChargeCalculator {
+//   constructor(customer, usage, provider) {
+//     this._customer = customer;
+//     this._usage = usage;
+//     this._provider = provider;
+//   }
+
+//   get charge() {
+//     const baseCharge = this._customer.baseRate * this._usage;
+//     return baseCharge + this._provider.connectionCharge;
+//   }
+// }
+
+export function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
