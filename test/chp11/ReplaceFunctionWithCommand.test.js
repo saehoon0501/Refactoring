aa8ef6c04114e13e6f978fbdb53da81d54d13722
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-16 23:14:37 +0900

    함수를 명령으로 바꾸기 setup

---
+++ test/chp11/ReplaceFunctionWithCommand.test.js
@@ -0,0 +1,59 @@
+import { score, Scorer } from "../../src/chp11/ReplaceFunctionWithCommand";
+
+describe("score", () => {
+  it("gives a score to a smoker with low original state", () => {
+    const lowCertificationStateCandidate = {
+      originState: 4,
+    };
+
+    const smokerMedicalExam = {
+      isSmoker: true,
+    };
+
+    expect(
+      score(lowCertificationStateCandidate, smokerMedicalExam, new Scorer())
+    ).toEqual(-10);
+  });
+
+  it("gives a score to a smoker with high original state", () => {
+    const lowCertificationStateCandidate = {
+      originState: 5,
+    };
+
+    const smokerMedicalExam = {
+      isSmoker: true,
+    };
+
+    expect(
+      score(lowCertificationStateCandidate, smokerMedicalExam, new Scorer())
+    ).toEqual(-5);
+  });
+
+  it("gives a score to a NON-smoker with low original state", () => {
+    const lowCertificationStateCandidate = {
+      originState: 4,
+    };
+
+    const smokerMedicalExam = {
+      isSmoker: false,
+    };
+
+    expect(
+      score(lowCertificationStateCandidate, smokerMedicalExam, new Scorer())
+    ).toEqual(-5);
+  });
+
+  it("gives a score to a NON-smoker with hight original state", () => {
+    const lowCertificationStateCandidate = {
+      originState: 5,
+    };
+
+    const smokerMedicalExam = {
+      isSmoker: false,
+    };
+
+    expect(
+      score(lowCertificationStateCandidate, smokerMedicalExam, new Scorer())
+    ).toEqual(0);
+  });
+});
