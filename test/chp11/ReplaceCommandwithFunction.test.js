545eb632c21e9ddd210cf8374ac94c19b023a359
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-16 23:33:48 +0900

    명령을 함수로 바꾸기 setup

---
+++ test/chp11/ReplaceCommandwithFunction.test.js
@@ -0,0 +1,10 @@
+import { charge } from "../../src/chp11/ReplaceCommandwithFunction";
+
+describe("ChargeCalculator", () => {
+  const customer = { baseRate: 0.1 };
+  const provider = { connectionCharge: 1000 };
+
+  it("should calculate total  charge", () => {
+    expect(charge(customer, 100, provider)).toEqual(1010);
+  });
+});
